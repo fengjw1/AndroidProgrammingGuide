2606ed306d17509340f6de5328074b2175871325
fengjw <fengjwxx@163.com>
2017-08-22 22:51:04 +0800

    today commit

--- RecyclerViewDemo/app/src/main/java/fengjw/com/recyclerviewdemo/NodeListFragment.java
+++ RecyclerViewDemo/app/src/main/java/fengjw/com/recyclerviewdemo/NodeListFragment.java
@@ -96,7 +96,7 @@ public class NodeListFragment extends Fragment{
 
         @Override
         public void onClick(View view) {
-            Toast.makeText(getActivity(), "onClick", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getActivity(), "onClick " + mNode.getId(), Toast.LENGTH_SHORT).show();
         }
     }
 
